0ffa79608c872306f2a476f665c4a1d8e43739b2
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-10-11 17:36:06 +0300

    updated the models

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
 
 
-class AutoDateTimeField(models.DateTimeField):
-    def pre_save(self, model_instance, add):
-        return timezone.now()
+# class AutoDateTimeField(models.DateTimeField):
+#     def pre_save(self, model_instance, add):
+#         return timezone.now()
 
 
 class Post(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(max_length=None)
     opening_time = models.DateField()
     closing_time = models.DateField()
-    created_at = models.DateField(default=timezone.now)
+    created_at = models.DateField(auto_now=True)
